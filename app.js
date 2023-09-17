5ce48b7b390fa63347027130257270dfdb5e827f
monti242421 <monti242421@gmail.com>
2023-09-17 14:25:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
-    process.exit();
+    //process.exit();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My Fisrt Page</title></head>')
+    res.write('<body><h1>Hello from my node server</h1></body>')
+    res.write('</html>')
+    res.end();
 })
 
 server.listen(4000);
